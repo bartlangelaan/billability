f3c2b16506486b067f06a314b5273b052957578e
Bart Langelaan <langelaanbart@gmail.com>
2017-04-08 14:23:26 +0200

    Changed the loading screen for a spinner

--- src/components/LoadingScreen/index.js
+++ src/components/LoadingScreen/index.js
@@ -3,7 +3,9 @@ import styles from './styles.css';
 
 function LoadingScreen() {
   return <div className={styles.wrapper}>
-    Loading...
+    <div className={styles.spinner}>
+      <div /><div /><div /><div /><div />
+    </div>
   </div>
 }
 
