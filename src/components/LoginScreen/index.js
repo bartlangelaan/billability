1b68f146ca405cd56249dc786a2fb2ae19365e76
Bart Langelaan <langelaanbart@gmail.com>
2017-04-08 13:46:54 +0200

    Add new favicon

--- src/components/LoginScreen/index.js
+++ src/components/LoginScreen/index.js
@@ -18,6 +18,9 @@ function LoginScreen() {
           </Button>
         </p>
       </div>
+      <div className={styles.footer}>
+        <p>Favicon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">flaticon.com</a>, licensed <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>.</p>
+      </div>
     </div>
   );
 }
