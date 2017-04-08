b6d0aa604c8a7a5aef4895e77b2a3c369f4c95ed
Bart Langelaan <langelaanbart@gmail.com>
2017-04-08 12:03:09 +0200

    Show time and date of last refresh

--- src/components/DashboardPage/index.js
+++ src/components/DashboardPage/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import AppBar from 'react-toolbox/lib/app_bar';
 import Button from 'react-toolbox/lib/button';
+import Tooltip from 'react-toolbox/lib/tooltip';
 import styles from './styles.css';
 import classnames from 'classnames';
 import data from '../data';
 import { REFRESH_STEPS } from '../const';
+const TooltipButton = Tooltip(Button);
 
 function DashboardPage({children, contentClassName}) {
+  const refreshing = data.state >= REFRESH_STEPS.INIT && data.state < REFRESH_STEPS.DONE;
   return (
     <div className={styles.wrapper}>
       <AppBar title="Billability">
-        <Button primary raised inverse href="/api/refresh" disabled={data.state >= REFRESH_STEPS.INIT && data.state < REFRESH_STEPS.DONE}>
+        <TooltipButton
+          primary raised inverse
+          href="/api/refresh"
+          disabled={refreshing}
+          tooltip={refreshing ? 'Refreshing right now..' : (data.timestamp ? `Last refresh: ${data.timestamp.toLocaleDateString()} ${data.timestamp.toLocaleTimeString()}` : 'Last refresh time unknown')}
+        >
           Refresh
-        </Button>
+        </TooltipButton>
         &nbsp;
         <Button flat inverse href="/logout">
           Log out
