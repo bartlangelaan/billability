9acc2d139b09866996cd1edad7d93fe1484ce9e9
Bart Langelaan <langelaanbart@gmail.com>
2017-04-08 12:55:54 +0200

    Save all settings in localStorage

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,11 +1,18 @@
-import { observable, extendObservable } from 'mobx';
+import { observable, extendObservable, autorun } from 'mobx';
 import { BILLABILITY_TYPE, GROUP_TYPES } from './const';
 
 const settings = observable({
-  billabilityType: BILLABILITY_TYPE.HOURS_REGISTERED_BILLABLE_PROJECT,
-  groupType: GROUP_TYPES.COUNTABLE,
-  from: '2016-50',
-  to: '2018-01'
+  billabilityType: localStorage.getItem('settings.billabilityType') || BILLABILITY_TYPE.HOURS_REGISTERED_BILLABLE_PROJECT_NOT_OVERSPENT,
+  groupType: localStorage.getItem('settings.groupType') || GROUP_TYPES.COUNTABLE,
+  from: localStorage.getItem('settings.from') || '2016-50',
+  to: localStorage.getItem('settings.to') || '2018-01',
+});
+
+autorun(() => {
+  localStorage.setItem('settings.billabilityType', settings.billabilityType);
+  localStorage.setItem('settings.groupType', settings.groupType);
+  localStorage.setItem('settings.from', settings.from);
+  localStorage.setItem('settings.to', settings.to);
 });
 
 export default settings;
